fd01591d479008bed669204882d548072a454d32
unknown <hosiniiiimehdi7412020@gmail.com>
2024-07-27 15:17:35 +0330

    add some things

--- js/app.js
+++ js/app.js
@@ -14,11 +14,8 @@ document.addEventListener("DOMContentLoaded", () => {
       addTodoToDOM(newTodo);
       saveTodoToLocalStorage(newTodo);
       todoInput.value = "";
-    }
-    else{
-      alert(`hello 🤗
-Please do not leave it blank😤
-Please note that you cannot add an empty task to the task list. Submitting the form without entering text will register the list of contentless tasks that can be managed and sorted tasks. To improve the experience and maintain data quality, be sure to enter a valid title for each new task. It helps you manage your to-do list automatically and efficiently.`);
+    } else {
+      alert("Input field cannot be empty.");
     }
   });
 
@@ -30,12 +27,12 @@ Please note that you cannot add an empty task to the task list. Submitting the f
     const li = document.createElement("li");
     li.classList.add("todo-item", "gradient-text");
     li.innerHTML = `
-            ${todo}
-            <div class="buttons">
-                <span class="decorative-text">Delete</span>
-                <span class="decorative-text">Undo</span>
-            </div>
-        `;
+                    ${todo}
+                    <div class="buttons">
+                        <span class="decorative-text" onclick="deleteItem(this)">Delete</span>
+                        <span class="decorative-text" onclick="deleteItem(this)">Undo</span>
+                    </div>
+                `;
     todoListContainer.appendChild(li);
   }
 
@@ -44,3 +41,14 @@ Please note that you cannot add an empty task to the task list. Submitting the f
     localStorage.setItem("todos", JSON.stringify(savedTodos));
   }
 });
+
+function deleteItem(el) {
+  const item = el.closest("li");
+  const todoText = item.firstChild.textContent.trim();
+  let savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+
+  savedTodos = savedTodos.filter((todo) => todo !== todoText);
+  localStorage.setItem("todos", JSON.stringify(savedTodos));
+
+  el.parentNode.parentNode.remove();
+}
