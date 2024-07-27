58ef2530eb2037dfed54118384c18abd6599be85
unknown <hosiniiiimehdi7412020@gmail.com>
2024-07-27 15:02:36 +0330

    Fix some bug

--- js/app.js
+++ js/app.js
@@ -1,40 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const todoInput = document.getElementById("todo-input");
-//   const todoListContainer = document.getElementById("todo-list");
-//   const todoForm = document.getElementById("todo-form");
-
-//   let savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
-//   savedTodos.forEach((todo) => addTodoToDOM(todo));
-
-//   todoForm.addEventListener("submit", (event) => {
-//     event.preventDefault();
-//     const newTodo = todoInput.value.trim();
-
-//     if (newTodo) {
-//       addTodoToDOM(newTodo);
-//       saveTodoToLocalStorage(newTodo);
-//       todoInput.value = "";
-//     }
-//   });
-
-//   function addTodoToDOM(todo) {
-//     const li = document.createElement("li");
-//     li.classList.add("todo-item");
-//     li.innerHTML = `
-//             ${todo}
-//             <div class="buttons">
-//                 <span class="decorative-text">Delete</span>
-//                 <span class="decorative-text">Undo</span>
-//             </div>
-//         `;
-//     todoListContainer.appendChild(li);
-//   }
-
-//   function saveTodoToLocalStorage(todo) {
-//     savedTodos.push(todo);
-//     localStorage.setItem("todos", JSON.stringify(savedTodos));
-//   }
-// });
 document.addEventListener("DOMContentLoaded", () => {
   const todoInput = document.getElementById("todo-input");
   const todoListContainer = document.getElementById("todo-list");
@@ -51,6 +14,11 @@ document.addEventListener("DOMContentLoaded", () => {
       addTodoToDOM(newTodo);
       saveTodoToLocalStorage(newTodo);
       todoInput.value = "";
+    }
+    else{
+      alert(`hello 🤗
+Please do not leave it blank😤
+Please note that you cannot add an empty task to the task list. Submitting the form without entering text will register the list of contentless tasks that can be managed and sorted tasks. To improve the experience and maintain data quality, be sure to enter a valid title for each new task. It helps you manage your to-do list automatically and efficiently.`);
     }
   });
 
